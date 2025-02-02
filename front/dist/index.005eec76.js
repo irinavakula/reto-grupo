fa5eafead5cf076ebcb49cc8756ac85a0087d6cd
irinavakula <irinkavakula@gmail.com>
2025-02-02 14:51:33 +0100

    Add init functionality (#1)
    * part 1
    
    * add gitigonre

--- front/dist/index.005eec76.js
+++ front/dist/index.005eec76.js
@@ -1,140 +1,79 @@
 $(function() {
     function getPartidos() {
-        $('tbody').empty();
-        $('zonaDetalle').hide();
-        $.ajax({
-            url: "http://localhost:1234/api/partidos",
-            success: function(data) {
-                data.forEach((element)=>{
-                    $('tbody').append($('<tr>').append($('<td>').text(element.nombre)).append($('<td>').text(element.deporte)).append($('<td>').attr('class', 'precio').text(parseFloat(element.apuesta).toFixed(2) + " \u20AC")).append($('<td>').text(mostrarResultado(element.resultado))).append($('<td>').attr('class', 'tdBotones').append($('<button>').addClass('btn btn-dark btn-sm botonMaestro ganar').attr('data-id', element.id).text('Ganar'))).append($('<td>').attr('class', 'tdBotones').append($('<button>').addClass('btn btn-dark btn-sm botonMaestro perder').attr('data-id', element.id).text('Perder'))).append($('<td>').attr('class', 'tdBotones').append($('<button>').addClass('btn btn-dark btn-sm botonMaestro borrar').attr('data-id', element.id).text('Borrar'))).append($('<td>').attr('class', 'tdBotones').append($('<button>').addClass('btn btn-dark btn-sm botonMaestro detalle').attr('data-id', element.id).text('Ver Detalle'))));
-                });
-            },
-            error: function(error) {
-                mostrarError("Error al listar los productos");
-                console.log(error);
-            },
-            complete: function(data) {
-                $('tbody').off('click', '.botonMaestro.borrar');
-                $('tbody').off('click', '.botonMaestro.ganar');
-                $('tbody').off('click', '.botonMaestro.perder');
-                $('tbody').off('click', '.botonMaestro.detalle');
-                $('tbody').on('click', '.botonMaestro.borrar', function(event) {
-                    event.stopPropagation();
-                    var id = $(this).data('id');
-                    borrarPartido(id);
-                    console.log('Hola desde botonBorrar');
-                });
-                $('tbody').on('click', '.botonMaestro.ganar', function(event) {
-                    event.stopPropagation();
-                    var id = $(this).data('id');
-                    setTimeout(ganar(id), 1000);
-                });
-                $('tbody').on('click', '.botonMaestro.perder', function(event) {
-                    event.stopPropagation();
-                    var id = $(this).data('id');
-                    setTimeout(perder(id), 3000);
-                });
-                $('tbody').on('click', '.botonMaestro.detalle', function(event) {
-                    event.stopPropagation();
-                    var id = $(this).data('id');
-                    console.log('Hola desde boton VerDetalle');
-                    verDetalleProducto(id);
-                    $('#tituloDetalle').text('Detalles del producto');
-                    $('#botonCambios').text('Modificar').off().on('click', function() {
-                        if (verificarCampos()) mostrarModalConfirmacion('Modificar producto', "\xbfModificar producto?", ()=>{
-                            guardarDatos();
-                        });
-                    });
-                });
-            }
+        $.get("http://localhost:1234/api/partidos").done(function(data) {
+            var table = $(".table-group-divider");
+            console.log(data);
+            data.forEach((partido)=>{
+                const row = `
+          <tr>
+            <td>${partido.nombre}</td>            
+            <td>${partido.deporte}</td>
+            <td>${partido.apuesta}</td>
+            <td>${partido.resultado}</td>
+            <td><button id="detalle" data-id=${partido.id}>Detalle</button></td>      
+            <td><button id="borrar" data-id=${partido.id}>Borrar</button></td> 
+            <td><button id="ganar" data-id=${partido.id}>Ganar</button></td> 
+            <td><button id="perder"data-id=${partido.id}>Pierde</button></td>
+            <td><button id="modificar"data-id=${partido.id}>Modificar</button></td>
+          </tr>          
+        `;
+                table.append(row);
+            });
+            console.log($("#detalle"));
+        }).fail(function() {
+            alert("Error ok");
         });
     }
-    function mostrarResultado(resultado) {
-        switch(resultado){
-            case 1:
-                return "Victoria";
-            case -1:
-                return "Derrota";
-            case 0:
-                return "Empate";
-            default:
-                return "error";
-        }
-    }
-    function borrarPartido(id) {
-        $.ajax({
-            url: `http://localhost:1234/api/partidos/${id}`,
-            type: 'DELETE',
-            success: function(data) {
-                refrescar();
-            },
-            error: function(error) {
-                //mostrarError("Fallo al eliminar el partido")
-                console.log(error);
-            }
-        });
-    }
-    function verDetalleProducto(id) {
-        $.ajax({
-            url: `http://localhost:1234/api/partidos/${id}`,
-            success: function(data) {
-                $('#id').val(data.id);
-                $('#nombrePartido').val(data.nombre);
-                $('#descripcionPartido').val(data.descripcion);
-                $('#deporte').val(data.deporte);
-                $('#resultado').val(mostrarResultado(data.resultado));
-                $('#apuestaPartido').val(parseFloat(data.apuesta).toFixed(2) + " \u20AC");
-                if (data.id >= 0) {
-                    mostrarDetalle();
-                    $('#botonCambios').text('Modificar');
-                }
-            },
-            error: function(error) {
-                mostrarError("Error al recuperar los datos");
-                console.log(error);
-            }
-        });
-    }
-    function ganar(id) {
-        $.ajax({
-            url: `http://localhost:1234/api/partidos/${id}/gana`,
-            type: "POST",
-            success: function(respuesta) {},
-            error: function(error) {
-                console.log(error.responseText);
-            },
-            complete: function() {
-                refrescar();
-            }
-        });
-    }
-    function perder(id) {
-        $.ajax({
-            url: `http://localhost:1234/api/partidos/${id}/pierde`,
-            type: "POST",
-            success: function(respuesta) {},
-            error: function(error) {
-                console.log(error.responseText);
-            },
-            complete: function() {
-                refrescar();
-            }
-        });
-    }
-    function mostrarDetalle() {
-        $('.zonaDetalle').show();
-    //$('button.botonMaestro').prop('disabled', true);
-    }
-    function refrescar() {
-        $('.zonaDetalle').hide();
-        //$('#botonCambios').off()
+    function refreshPartidos() {
+        var table = $(".table-group-divider");
+        table.empty();
         getPartidos();
     }
-    getPartidos();
-    $('#refrescar').on('click', function() {
-        refrescar();
+    refreshPartidos();
+    $(document).on("click", "#detalle", function() {
+        const partidoId = $(this).data("id");
+        console.log(partidoId);
+        $.get("http://localhost:1234/api/partidos/" + partidoId).done(function(data) {
+            $("#id").val(data.id);
+            $("#nombrePartido").val(data.nombre);
+            $("#descripcionPartido").val(data.descripcion);
+            $("#deporte").val(data.deporte);
+            $("#resultado").val(data.resultado);
+            $("#apuestaPartido").val(data.apuesta);
+            $(".zonaDetalle").show();
+        }).fail(function() {
+            alert("Lox");
+        });
     });
-    $('.zonaDetalle').hide();
+    $(document).on("click", "#botonCancelar", function() {
+        $(".zonaDetalle").hide();
+    });
+    $(document).on("click", "#borrar", function() {
+        const partidoId = $(this).data("id");
+        console.log(partidoId);
+        $.ajax({
+            method: "DELETE",
+            url: "http://localhost:1234/api/partidos/" + partidoId
+        }).done(function(data) {
+            console.log("Resultado del borrado: " + data);
+            refreshPartidos();
+        });
+    });
+    $(document).on("click", "#refrescar", function() {
+        refreshPartidos();
+    });
+    $(document).on("click", "#ganar", function() {
+        const partidoId = $(this).data("id");
+        const gana = $(this).data();
+        $.post("http://localhost:1234/api/partidos/" + partidoId + "/gana");
+        refreshPartidos();
+    });
+    $(document).on("click", "#perder", function() {
+        const partidoId = $(this).data("id");
+        const pierde = $(this).data();
+        $.post("http://localhost:1234/api/partidos/" + partidoId + "/pierde");
+        refreshPartidos();
+    });
 });
 
 //# sourceMappingURL=index.005eec76.js.map
